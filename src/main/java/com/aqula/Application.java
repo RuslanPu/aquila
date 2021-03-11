c6a10b4ff1aa87ba25f8952c3bab7815dcdbbb65
Ruslan <ruslanpunin@yande.ru>
2021-03-11 17:28:01 +0300

    example in-memory spring security

---
+++ src/main/java/com/aqula/Application.java
@@ -0,0 +1,34 @@
+package com.aqula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@SpringBootApplication
+public class Application implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public RestTemplate getRestTemplate() { return new RestTemplate(); }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+
+}
---
+++ src/main/java/com/aqula/config/SpringSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.aqula.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // роль admin всегда есть доступ к /admin/**
+    // роль user всегда есть доступ к /user/**
+    // Наш кастомный "403 access denied" обработчик
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/", "/index", "/about").permitAll()
+//                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+//                .defaultSuccessUrl("/greeting")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+
+    }
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("pas")
+                .authorities("ROLE_ADMIN")
+                .and()
+                .withUser("user")
+                .password("pas").authorities("ROLE_USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/images/**","/vendor/**","/fonts/**","/other/**","css/**","js/**","images/**","webfonts/**");
+    }
+
+    // создаем пользоватлелей, admin и user
+
+
+
+}
